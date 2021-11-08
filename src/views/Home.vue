2d11474fd71a3128e57125516bc17d1c15f0b4ef
jeffubayi <jeffubayi@gmail.com>
2021-11-08 06:32:54 +0300

    home -add function to get all lists witihin the app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,29 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import { supabase } from "../supabase/init";
 export default {
-  name: "Home",
+  name: "home",
   components: {},
   setup() {
     // Create data / vars
-
+    const data = ref([]);
+    const dataLoaded = ref(null);
     // Get data
-
+    const getData = async () => {
+      try {
+        const { data: workouts, error } = await supabase.from("workouts").select("*");
+        if (error) throw error;
+        data.value = workouts;
+        dataLoaded.value = true;
+      } catch (error) {
+        console.warn(error.message);
+      }
+    };
     // Run data function
-
-    return {};
+    getData();
+    return { data, dataLoaded };
   },
 };
 </script>
