69737dc9a6f9ae7376afd334d3de690cb8d5c498
jeffubayi <jeffubayi@gmail.com>
2021-11-08 06:08:53 +0300

    create - add create ops for adding shopping

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -3,10 +3,17 @@
 </template>
 
 <script>
+import {ref} from "vue";
+
 export default {
   name: "create",
   setup() {
     // Create data
+    const workoutName = ref("");
+    const workoutType = ref("select-workout");
+    const exercises = ref([]);
+    const statusMsg = ref(null);
+    const errorMsg = ref(null);
 
     // Add exercise
 
