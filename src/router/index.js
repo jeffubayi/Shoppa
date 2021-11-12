1f57ec00a3c50710e050c2b81267be12ea8007c3
jeffubayi <jeffubayi@gmail.com>
2021-11-12 20:14:55 +0300

    routes - import  components to route page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { supabase } from "../supabase/init";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Create from "../views/Create.vue";
+import ViewWorkout from "../views/ViewWorkout.vue";
 
 const routes = [
   {
