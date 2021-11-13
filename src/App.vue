d22fb277c2042ab3806bdedbadafd69a5d662319
jeffubayi <jeffubayi@gmail.com>
2021-11-13 20:31:43 +0300

    app - import supabase ,navigation ,store

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
 </template>
 
 <script>
+import Navigation from "./components/Navigation.vue";
+import { ref } from "vue";
+import { supabase } from "./supabase/init";
+import store from "./store/index";
 export default {
   setup() {
     // Create data / vars
