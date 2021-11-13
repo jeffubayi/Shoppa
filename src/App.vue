7220962517760558379a17372f632eb58b3e53ea
jeffubayi <jeffubayi@gmail.com>
2021-11-13 20:33:22 +0300

    app - add a function to check if customer already logged in

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,9 @@ export default {
   },
   setup() {
     // Create data / vars
-
+    const appReady = ref(null);
     // Check to see if user is already logged in
+    const user = supabase.auth.user();
 
     // If user does not exist, need to make app ready
 
