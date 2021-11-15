8965496526e06b21f439166a43690816ef6da962
jeffubayi <jeffubayi@gmail.com>
2021-11-15 03:32:29 +0300

    update tailwind color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "at-light-green": "#44ba9f",
-        "at-green": "#286d5d",
+        "at-light-green": "#06B6D4",
+        "at-green": "#06B6D4",
         "light-grey": "#f1f1f1",
       },
     },
@@ -13,8 +13,8 @@ module.exports = {
       Poppins: ["Poppins, sans-serif"],
     },
     flex: {
-      "1": "1 1 0%",
-      "2": "2 2 0%",
+      1: "1 1 0%",
+      2: "2 2 0%",
     },
     container: {
       center: true,
