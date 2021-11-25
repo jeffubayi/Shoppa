2c8b86af4da587a4a38019d3bdf9bd342ff8928f
jeffubayi <jeffubayi@gmail.com>
2021-11-25 04:59:17 +0300

    improve  the homepage ui/ux

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,96 +21,83 @@
     </div>
 
     <!-- Data -->
-    <section class="text-gray-600 body-font" v-else>
-  <div class="container px-5 py-24 mx-auto">
-     <div class="max-w-sm rounded overflow-hidden shadow-lg " >
+    <div
+      class="
+        grid grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-3
+        gap-6
+      "
+      v-else
+    >
       <router-link
-        class="
-          flex flex-col
-          items-center
-          p-8
-          shadow-md
-          cursor-pointer
-          mb-3
-          mx-auto
-        "
+        class="flex flex-col items-center p-8 cursor-pointer mb-3"
         :to="{ name: 'View-Workout', params: { workoutId: workout.id } }"
         v-for="(workout, index) in data"
         :key="index"
       >
-        <img
-          class="w-full"
-          src="https://www.lux-review.com/wp-content/uploads/2020/12/grocery-shopping.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{{ workout.workoutName }}</div>
+        <!-- Card -->
+        <div class="" style="width: 350px">
+          <img
+            src="https://www.lux-review.com/wp-content/uploads/2020/12/grocery-shopping.jpg"
+            class="rounded-lg shadow-xl mb-4"
+            alt=""
+          />
+
+          <div class="text-center">
+            <h5 class="text-xl font-semibold mb-2">
+              {{ workout.workoutName }}
+            </h5>
+
+            <div class="px-6 pt-4 pb-2">
+              <span
+                class="
+                  inline-block
+                  bg-gray-200
+                  rounded-full
+                  px-3
+                  py-1
+                  text-sm
+                  font-semibold
+                  text-gray-700
+                  mr-2
+                  mb-2
+                "
+                ># {{ workout.workoutType }}</span
+              >
+            </div>
+
+            <button
+              class="
+                bg-at-light-green
+                text-white
+                active:bg-purple-600
+                font-bold
+                uppercase
+                text-xs
+                px-4
+                py-2
+                rounded
+                shadow
+                hover:shadow-md
+                outline-none
+                focus:outline-none
+                mr-1
+                mb-1
+                ease-linear
+                transition-all
+                duration-150
+              "
+              type="button"
+            >
+              View list
+            </button>
+          </div>
         </div>
-        <div class="px-6 pt-4 pb-2">
-          <span
-            class="
-              inline-block
-              bg-gray-200
-              rounded-full
-              px-3
-              py-1
-              text-sm
-              font-semibold
-              text-gray-700
-              mr-2
-              mb-2
-            "
-            ># {{ workout.workoutType }}</span
-          >
-        </div>
+        <!-- Card -->
       </router-link>
-    </div> 
-          
-  </div>
-</section>
-    <!-- Card -->
-<!-- <div class="" style="width: 350px">
-  <img
-    src="https://www.lux-review.com/wp-content/uploads/2020/12/grocery-shopping.jpg"
-    class="rounded-lg shadow-xl mb-4"
-    alt=""
-  />
-
-  <div class="text-center">
-    <h5 class="text-xl font-semibold mb-2">Card title</h5>
-
-    <p class="mb-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    </p>
-
-    <button
-      class="
-        bg-purple-500
-        text-white
-        active:bg-purple-600
-        font-bold
-        uppercase
-        text-xs
-        px-4
-        py-2
-        rounded
-        shadow
-        hover:shadow-md
-        outline-none
-        focus:outline-none
-        mr-1
-        mb-1
-        ease-linear
-        transition-all
-        duration-150
-      "
-      type="button"
-    >
-      Button
-    </button>
-  </div>
-</div> -->
-<!-- Card -->
+    </div>
   </div>
 </template>
 
