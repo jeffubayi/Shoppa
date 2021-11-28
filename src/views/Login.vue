3758fd4491b9ea254790a964a5517a8162afcf4b
jeffubayi <jeffubayi@gmail.com>
2021-11-28 22:16:14 +0300

    update the login ui

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,60 +6,187 @@
     </div>
 
     <!-- Login -->
-    <form
-      @submit.prevent="login"
-      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
-    >
-      <h1 class="text-3xl text-at-light-green mb-4">Login</h1>
-
-      <div class="flex flex-col mb-2">
-        <label for="email" class="mb-1 text-sm text-at-light-green"
-          >Email</label
-        >
-        <input
-          type="text"
-          required
-          class="p-2 text-gray-500 focus:outline-none"
-          id="email"
-          v-model="email"
-        />
-      </div>
-
-      <div class="flex flex-col mb-2">
-        <label for="password" class="mb-1 text-sm text-at-light-green"
-          >Password</label
-        >
-        <input
-          type="password"
-          required
-          class="p-2 text-gray-500 focus:outline-none"
-          id="password"
-          v-model="password"
-        />
-      </div>
-
-      <button
-        type="submit"
+    <div class="p-3 flex flex-col bg-white">
+      <div
         class="
-          mt-6
-          py-2
-          px-6
-          rounded-sm
-          self-start
-          text-sm text-white
-          bg-at-light-green
-          duration-200
-          border-solid border-2 border-transparent
-          hover:border-at-light-green hover:bg-white hover:text-at-light-green
+          flex flex-col
+          bg-white
+          shadow-md
+          px-4
+          sm:px-6
+          md:px-8
+          lg:px-10
+          py-8
+          rounded-3xl
+          w-50
+          max-w-md
         "
       >
-        Login
-      </button>
+        <div class="font-medium self-center text-xl sm:text-3xl text-gray-800">
+          Login
+        </div>
+        <div class="mt-4 self-center text-xl sm:text-sm text-gray-800">
+          Enter your credentials to access your Shoppa list
+        </div>
 
-      <router-link class="text-sm mt-6 text-center" :to="{ name: 'Register' }">
-        Don't have an account? <span class="text-at-light-green">Register</span>
-      </router-link>
-    </form>
+        <div class="mt-10">
+          <form @submit.prevent="login">
+            <div class="flex flex-col mb-5">
+              <label
+                for="email"
+                class="mb-1 text-xs tracking-wide text-gray-600"
+                >E-Mail Address:</label
+              >
+              <div class="relative">
+                <div
+                  class="
+                    inline-flex
+                    items-center
+                    justify-center
+                    absolute
+                    left-0
+                    top-0
+                    h-full
+                    w-10
+                    text-gray-400
+                  "
+                >
+                  <i class="fas fa-at text-blue-500"></i>
+                </div>
+
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  required
+                  v-model="email"
+                  class="
+                    text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    rounded-2xl
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-blue-400
+                  "
+                  placeholder="Enter your email"
+                />
+              </div>
+            </div>
+            <div class="flex flex-col mb-6">
+              <label
+                for="password"
+                class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+                >Password:</label
+              >
+              <div class="relative">
+                <div
+                  class="
+                    inline-flex
+                    items-center
+                    justify-center
+                    absolute
+                    left-0
+                    top-0
+                    h-full
+                    w-10
+                    text-gray-400
+                  "
+                >
+                  <span>
+                    <i class="fas fa-lock text-blue-500"></i>
+                  </span>
+                </div>
+
+                <input
+                  id="password"
+                  type="password"
+                  required
+                  name="password"
+                  v-model="password"
+                  class="
+                    text-sm
+                    placeholder-gray-500
+                    pl-10
+                    pr-4
+                    rounded-2xl
+                    border border-gray-400
+                    w-full
+                    py-2
+                    focus:outline-none focus:border-blue-400
+                  "
+                  placeholder="Enter your password"
+                />
+              </div>
+            </div>
+
+            <div class="flex w-full">
+              <button
+                type="submit"
+                class="
+                  flex
+                  mt-2
+                  items-center
+                  justify-center
+                  focus:outline-none
+                  text-white text-sm
+                  sm:text-base
+                  bg-at-light-green
+                  hover:bg-at-light-green
+                  rounded-2xl
+                  py-2
+                  w-full
+                  transition
+                  duration-150
+                  ease-in
+                "
+              >
+                <span class="mr-2 uppercase">Sign Up</span>
+                <span>
+                  <svg
+                    class="h-6 w-6"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="flex justify-center items-center mt-6">
+        <a
+          href="#"
+          target="_blank"
+          class="
+            inline-flex
+            items-center
+            text-gray-700
+            font-medium
+            text-xs text-center
+          "
+        >
+          <span class="ml-2"
+            >Don't have an account?
+            <router-link
+              class="text-xs ml-2 text-at-light-green font-semibold"
+              :to="{ name: 'Register' }"
+              >Register here</router-link
+            ></span
+          >
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
